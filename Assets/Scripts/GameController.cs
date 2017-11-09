b1896a014c7ce49065005f596c6e857f4287472f
AdamPiech <adampiech7@op.pl>
2017-11-09 19:05:32 +0100

    Init project and created projectr sceleton

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    private System.Random rand = new System.Random();
+    public Button[] worms;
+
+    void Start()
+    {
+        killAllWorms();
+    }
+
+    void Update()
+    {
+        if (rand.Next(175) == 0)
+        {
+            InvokeRepeating("createWorm", 5, 0);
+        }
+    }
+
+    void createWorm()
+    {
+        worms[rand.Next(16)].GetComponent<WormScript>().createWorm();
+    }
+
+    private void killAllWorms()
+    {
+        for (int index = 0; index < worms.Length; index++)
+        {
+            worms[index].GetComponent<WormScript>().killWorm();
+        }
+    }
+
+}
---
+++ Assets/Scripts/WormScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WormScript : MonoBehaviour {
+
+    private GameController gameController;
+    public Button wormButton;
+    public Sprite worm;
+
+    private float createWormTime;
+
+    void Update()
+    {
+        if(Time.time - createWormTime > 3)
+        {
+            killWorm();
+        }
+    }
+
+    public void createWorm()
+    {
+        wormButton.GetComponent<Image>().sprite = worm;
+        wormButton.interactable = true;
+        getCreatedTime();
+    }
+
+    public void killWorm()
+    {
+        wormButton.GetComponent<Image>().sprite = null;
+        wormButton.interactable = false;
+    }
+
+    private void getCreatedTime()
+    {
+        createWormTime = Time.time;
+    }
+
+}
